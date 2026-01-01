5d7f9372da85a389358d46e7c272c6034eb02c38
اسمك <>
2026-01-01 20:27:57 +0200

    update project data

--- src/componets/subjects/math/math.jsx
+++ src/componets/subjects/math/math.jsx
@@ -1,14 +1,46 @@
+import { useState } from "react";
+import { Pvideos } from "../../../constant/data";
+import ReactPlayer from "react-player";
+import {
+  MediaController,
+  MediaControlBar,
+  MediaTimeRange,
+  MediaTimeDisplay,
+  MediaVolumeRange,
+  MediaPlaybackRateButton,
+  MediaPlayButton,
+  MediaSeekBackwardButton,
+  MediaSeekForwardButton,
+  MediaMuteButton,
+  MediaFullscreenButton,
+} from "media-chrome/react";
+
+
+
 const Math = () => {
+  const [show , setshow] = useState("lectures")
   // استيراد كل ملفات PDF من نفس الفولدر
+
+  return (
+    <div className="flex items-center  text-white justify-center flex-col">
+      <div className=" items-center text my-2 justify-around  cursor-pointer  mt-3 w-50 px-4 h-10 bg-blue-100/10 rounded-full flex   ">
+        <button onClick={()=>setshow("videos")} className={`${show==="videos"?"active-btn":""}`} >Videos</button>
+        <button onClick={()=>setshow("lectures")}  className={`${show==="lectures"?"active-btn":""}`} >lectures</button>
+      </div>
+      {show === "lectures" &&<Lectures/> }
+      {show === "videos"&&<Videos/> }
+    </div>
+  );
+};
+function Lectures (){
   const pdfFiles = import.meta.glob("./*.pdf", { eager: true });
 
   // تحويلهم لـ array + ترتيبهم أبجديًا
   const pdfs = Object.entries(pdfFiles).sort(([a], [b]) =>
     a.localeCompare(b)
   );
-
-  return (
-    <div className="container w-90 max-lg:w-60 pt-2 bg-white/5 text-white mt-5 rounded-lg transition duration-300 ease-in-out">
+  return(
+    <div className="container w-90 max-lg:w-60 pt-2 py-4 bg-white/5 text-white mt-5 rounded-lg transition duration-300 ease-in-out">
       {pdfs.map(([path, file], index) => {
         const fileName = path
           .split("/")
@@ -32,7 +64,47 @@ const Math = () => {
         );
       })}
     </div>
-  );
-};
+  )
+}
 
+function Videos (){
+ {/* return(
+    <div>
+      {Pvideos.map((video)=>(
+        <div className=" items-center rounded-2xl py-2 my-2 px-4 bg-blue-200/30" key={video.id}>
+          <p className="text-blue-200 text-center font-bold">{video.title}</p>
+
+    <MediaController
+      style={{
+        width: "100%",
+        aspectRatio: "16/9",
+      }}
+    >
+      <ReactPlayer
+        slot="media"
+       src={video.link}
+        controls={false}
+        style={{
+          width: "100%",
+          height: "100%",
+          "--controls": "none",
+        }}
+      ></ReactPlayer>
+      <MediaControlBar>
+        <MediaPlayButton />
+        <MediaSeekBackwardButton seekOffset={5} />
+        <MediaSeekForwardButton seekOffset={5} />
+        <MediaTimeRange />
+        <MediaTimeDisplay showDuration />
+        <MediaMuteButton />
+        <MediaVolumeRange />
+        <MediaPlaybackRateButton />
+        <MediaFullscreenButton />
+      </MediaControlBar>
+    </MediaController>
+    </div>
+      ))}
+  </div>
+  ); */}
+} 
 export default Math;
---
+++ src/constant/data.js
@@ -0,0 +1,105 @@
+export const test =[
+    {
+
+    },
+];
+export const Pvideos =[
+    {
+        id:1,
+        title:"chapter1 p1 Units",
+        time:"20:14",
+        link:"https://youtu.be/Ta0yuUu2hjk?si=5EltxgPk7aMz97Ai"
+    }
+       , {
+        id:2,
+        title:"chapter1 p2 Dimensions",
+        time:"15:06",
+        link:"https://youtu.be/L8RrSwIsDR0?si=4auxSa1L0ZTsQA-f"
+    }
+           , {
+        id:3,
+        title:"Chapter 2 part1 Newtons law of gravity and Kepler's Law",
+        time:"37:07",
+        link:"https://youtu.be/Mfew-u9AZuI?si=9dHG-3go7x-in3Lu"
+    }
+    , {
+        id:4,
+        title:"Chapter 2 part2 Gravitational Field",
+        time:"25:11",
+        link:"https://youtu.be/uNh3suzB8Ig?si=FPGkS4aDT2Q5xRK_"
+    }
+        , {
+        id:5,
+        title:"Ch3 part1 Pressure and Archimedes' principles",
+        time:"37:07",
+        link:"https://youtu.be/uNh3suzB8Ig?si=FPGkS4aDT2Q5xRK_"
+    }
+            , {
+        id:6,
+        title:"Ch3 part2 Surface tension Capillary action",
+        time:"35:12",
+        link:"https://www.youtube.com/watch?v=3MANV63kCdk&t=32s"
+    }
+                 , {
+        id:7,
+        title:"Ch3 part3 Continuity equation and Bernoulli equation",
+        time:"20:20",
+        link:"https://www.youtube.com/watch?v=3MANV63kCdk&t=32s"
+    }
+                     , {
+        id:8,
+        title:"Ch3 part4 viscosity poissuell stocks Raynold No",
+        time:"40:28",
+        link:"https://www.youtube.com/watch?v=kQX_4ESJJvM"
+    }
+                         , {
+        id:9,
+        title:"Ch4 Elasticity Young, shear and bulk modulus",
+        time:"40:28",
+        link:"https://www.youtube.com/watch?v=ZJeVdudQ7t4"
+    }
+                             , {
+        id:10,
+        title:"Ch5 Part1 Moment of inertia Parallel axis theorem",
+        time:"40:28",
+        link:"https://www.youtube.com/watch?v=W4pG8qM6iQ4"
+    }
+                                 , {
+        id:11,
+        title:"Ch6 Simple harmonic motion",
+        time:"40:28",
+        link:"https://www.youtube.com/watch?v=qKLc3Bw82fw"
+    }
+                                    , {
+        id:12,
+        title:"heat 1",
+        time:"40:28",
+        link:"https://www.youtube.com/watch?v=r3ltRATZbw0"
+    }
+                                        , {
+        id:13,
+        title:"heat 2",
+        time:"40:28",
+        link:"https://www.youtube.com/watch?v=AgLHkQuX5-4"
+    }
+                                            , {
+        id:14,
+        title:"heat 3",
+        time:"40:28",
+        link:"https://www.youtube.com/watch?v=3PkDOvpVWjY"
+    }
+                                                , {
+        id:15,
+        title:"heat 4",
+        time:"40:28",
+        link:"https://www.youtube.com/watch?v=IPHOnGlEntU"
+    }
+                                                   , {
+        id:16,
+        title:"heat 6",
+        time:"40:28",
+        link:"https://www.youtube.com/watch?v=ayhWaNXbsOw"
+    }
+    
+
+]
